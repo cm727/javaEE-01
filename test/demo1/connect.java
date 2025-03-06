ff94b8efc4c71c84c0e0462c43d031fccc18b61c
cmr <815797624@qq.com>
2025-03-06 22:19:20 +0800

    第一次提交

--- test/demo1/connect.java
+++ test/demo1/connect.java
@@ -5,9 +5,6 @@ package test.demo1;
  * Package: {PACKAGE_NAME}
  * Description:
  *
- * @Author cmr
- * @Create 2025/3/6   21:35
- * @Version 1.0
  */
 public class connect {
  public static void main(String[] args) {
