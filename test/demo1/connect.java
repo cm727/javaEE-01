6879c6686fafdd058de14f71006b7ee800f40d00
cmr <815797624@qq.com>
2025-03-06 22:18:11 +0800

    第一次提交

---
+++ test/demo1/connect.java
@@ -0,0 +1,16 @@
+package test.demo1;
+
+/**
+ * ClassName:connect
+ * Package: {PACKAGE_NAME}
+ * Description:
+ *
+ * @Author cmr
+ * @Create 2025/3/6   21:35
+ * @Version 1.0
+ */
+public class connect {
+ public static void main(String[] args) {
+  System.out.println("hello");
+ }
+}
